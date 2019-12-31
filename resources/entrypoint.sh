1d07d524121aa14ca0e44f7f2e63d38acc3c9d41
Alessandro Bellanda <a.bellanda@esconsulting.it>
2019-12-31 10:21:28 +0100

    fixed xdebug

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -4,4 +4,10 @@ sed -i -e "s/REMOTE_HOST/${DOCKER_HOST_IP}/" /usr/local/etc/php/conf.d/php.ini
 sed -i -e "s/REMOTE_PORT/${DOCKER_HOST_PORT}/" /usr/local/etc/php/conf.d/php.ini
 sed -i -e "s/ENABLE_XDEBUG/${ENABLE_XDEBUG}/" /usr/local/etc/php/conf.d/php.ini
 
+if [ 0 == "${ENABLE_XDEBUG}" ]; then
+    if [ -e /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
+        rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    fi
+fi
+
 /usr/bin/supervisord
